c71618865de5b36fd2961d9224af7c1b0bc8f7cf
Moises-csantos <155759707+Moises-csantos@users.noreply.github.com>
2026-09-25 18:37:48 -0300

    Update produto.js

--- js/produto.js
+++ js/produto.js
@@ -3,7 +3,7 @@
 // =====================================================
 
 const CONFIG_PRODUTO = {
-    API_URL: "http://127.0.0.1:5000"
+    API_URL: "https://aliancascamarsan1993.pythonanywhere.com"
 };
 
 
